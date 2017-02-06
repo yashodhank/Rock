b6674395c5110ece7f608da3b6459463c48f32cd
Mike Peterson <mike@sparkabilitygroup.com>
2017-02-06 12:49:10 -0700

    Merge branch 'develop' into feature-mp-bulkupdate

--- RockWeb/Blocks/Cms/UniversalSearch.ascx.cs
+++ RockWeb/Blocks/Cms/UniversalSearch.ascx.cs
@@ -221,18 +221,26 @@ namespace RockWeb.Blocks.Cms
             var indexDocumentType = indexDocumentEntityType.GetEntityType();
 
             var client = IndexContainer.GetActiveComponent();
-            var document = client.GetDocumentById( indexDocumentType, documentId.AsInteger() );
 
-            var documentUrl = document.GetDocumentUrl();
+            if ( indexDocumentType != null ) {
+                var document = client.GetDocumentById( indexDocumentType, documentId.AsInteger() );
 
-            if ( !string.IsNullOrWhiteSpace( documentUrl ) )
-            {
-                Response.Redirect( documentUrl );
+                var documentUrl = document.GetDocumentUrl();
+
+                if ( !string.IsNullOrWhiteSpace( documentUrl ) )
+                {
+                    Response.Redirect( documentUrl );
+                }
+                else
+                {
+                    lResults.Text = "<div class='alert alert-warning'>No url is available for the provided index document.</div>";
+                }
             }
-            else
+            else 
             {
-                lResults.Text = "<div class='alert alert-warning'>No url is available for the provided index document.</div>";
+                lResults.Text = "<div class='alert alert-warning'>Invalid document type.</div>";
             }
+
         }
 
         /// <summary>
@@ -379,10 +387,12 @@ namespace RockWeb.Blocks.Cms
             }
             else
             {
-                // get entities from block config
-                if ( selectedEntities.Count == 0 )
+                selectedEntities = cblModelFilter.SelectedValuesAsInt;
+                
+                // if no entities from the UI get from the block config
+                if ( selectedEntities.Count == 0 && GetAttributeValue( "EnabledModels" ).IsNotNullOrWhitespace() )
                 {
-                    selectedEntities = cblModelFilter.SelectedValuesAsInt;
+                    selectedEntities = GetAttributeValue( "EnabledModels" ).Split( ',' ).Select( int.Parse ).ToList();
                 }
             }
 
@@ -418,9 +428,7 @@ namespace RockWeb.Blocks.Cms
             }
             else
             {
-                // get filters from the block config
-                
-                // add any base field filters
+                // add any base field filters from block settings
                 if ( !string.IsNullOrWhiteSpace( GetAttributeValue( "BaseFieldFilters" ) ) )
                 {
                     foreach ( var filterField in GetAttributeValue( "BaseFieldFilters" ).ToKeyValuePairList() )
@@ -502,6 +510,13 @@ namespace RockWeb.Blocks.Cms
 
             // add dynamic filters
             var selectedEntities = cblModelFilter.SelectedValuesAsInt;
+
+            // if no entities from the UI get from the block config
+            if ( selectedEntities.Count == 0 && GetAttributeValue( "EnabledModels" ).IsNotNullOrWhitespace() )
+            {
+                selectedEntities = GetAttributeValue( "EnabledModels" ).Split( ',' ).Select( int.Parse ).ToList();
+            }
+
             if ( selectedEntities.Count > 0 )
             {
                 foreach ( var control in phFilters.Controls )
@@ -619,6 +634,8 @@ namespace RockWeb.Blocks.Cms
             {
                 cblModelFilter.Visible = false;
             }
+
+            hrSeparator.Visible = cblModelFilter.Visible;
 
             ddlSearchType.BindToEnum<SearchType>();
             ddlSearchType.SelectedValue = GetAttributeValue( "SearchType" );
